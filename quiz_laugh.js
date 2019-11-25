01f97bfcd736b4bd5dab97291ec543c255ee32ed
vagrant <vagrant@vagrant.vm>
2019-11-25 04:05:31 +0000

    quiz laugh (5-4)

---
+++ quiz_laugh.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ Directions:
+
+Write an anonymous function expression that stores a function 
+in a variable called "laugh" and outputs the number of "ha"s 
+that you pass in as an argument.
+ */
+
+var laugh = function(num) {
+  var laugh = '';
+  for ( var i = 0; i < num; i ++) {
+    laugh += 'ha';
+  }
+      return laugh += '!';
+};
+
+console.log(laugh(10));
+
