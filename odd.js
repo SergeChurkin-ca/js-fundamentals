db4d7d67e3f596b6496584ef5e749eaa5bcf23a0
vagrant <vagrant@vagrant.vm>
2019-11-25 05:30:18 +0000

    Create a function to determine if a number is odd or not.

---
+++ odd.js
@@ -0,0 +1,13 @@
+/* 
+We have been hired to write a function that 
+can help with the mail sorting. In this challenge, 
+we will be writing a function to check whether a number is odd or not.
+*/
+
+function isOdd(num) {
+  return num % 2 === 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8))
+
