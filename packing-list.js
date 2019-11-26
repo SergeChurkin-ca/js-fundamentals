c4ab89cf0bfc173ac12cdba129a8c5ed845aa6a1
vagrant <vagrant@vagrant.vm>
2019-11-26 03:18:11 +0000

    Iterating Over Arrays. Assignment

---
+++ packing-list.js
@@ -0,0 +1,16 @@
+/*
+Iterating Over Arrays
+Assignment
+
+In this exercise, we're going to practice iterating over arrays with two common types of loops: the for and the while.
+
+*/
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+
